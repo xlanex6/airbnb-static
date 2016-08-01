1fec170705d287d61a20ae9aaae85d21afa26645
Alex <alexduval71@gmail.com>
2016-08-01 17:54:59 +0200

    finish with trips and  personnal trip

--- config.rb
+++ config.rb
@@ -27,6 +27,10 @@ activate :autoprefixer
 #   page "/admin/*"
 # end
 
+data.trips.each do |trip|
+  proxy "/trips/#{trip.country}.html", "/trips.html", locals: { trip: trip }
+end
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
